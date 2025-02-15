855f9bab66c84c75b6d327bd5ca05a2da7c8c687
murugan2678 <muruganm2947@gmail.com>
2025-02-15 19:07:53 +0530

    using_three_channel

---
+++ pwm/pwm_register_timer1_three_channel/Inc/arm.h
@@ -0,0 +1,111 @@
+/*
+ * arm.h
+ *
+ *  Created on: Feb 15, 2025
+ *      Author: murugan
+ */
+
+#ifndef ARM_H_
+#define ARM_H_
+
+/* structure struct struct_tag (or) struct name */
+struct rcc
+{
+	/* structure syntax data type member (or) field (or) elements */
+	/* RCC register 			  offset value */
+	unsigned int CR;                // 0x00
+	unsigned int PLLCFGR;           // 0x04
+	unsigned int CFGR;              // 0x08
+	unsigned int CIR;				// 0x0C
+	unsigned int AHB1RSTR;			// 0x10
+	unsigned int AHB2RSTR;			// 0x14
+	unsigned int rsd0;              // 0x18  Reserved address for rsd0
+	unsigned int rsd1;				// 0x1C	 Reserved address for rsd1
+	unsigned int APB1RSTR;			// 0x20
+	unsigned int APB2RSTR;			// 0x24
+	unsigned int rsd2;				// 0x28	 Reserved address for rsd2
+	unsigned int rsd3;				// 0x2C  Reserved address for rsd3
+	unsigned int AHB1ENR; 			// 0x30
+	unsigned int AHB2ENR; 			// 0x34
+	unsigned int rsd4;				// 0x38	 Reserved address for rsd4
+	unsigned int rsd5;				// 0x3C	 Reserved address for rsd5
+	unsigned int APB1ENR;			// 0x40
+	unsigned int APB2ENR;           // 0x44
+	unsigned int rsd6;				// 0x48	 Reserved address for rsd6
+	unsigned int rsd7;				// 0x4C	 Reserved address for rsd7
+	unsigned int AHB1LPENR;			// 0x50
+	unsigned int AHB2LPENR;			// 0x54
+	unsigned int rsd8;				// 0x58	 Reserved address for rsd8
+	unsigned int rsd9;				// 0x5C	 Reserved address for rsd9
+	unsigned int APB1LPENR;         // 0x60
+	unsigned int APB2LPENR;			// 0x64
+	unsigned int rsd10;				// 0x68	 Reserved address for rsd10
+	unsigned int rsd11;				// 0x6C	 Reserved address for rsd11
+	unsigned int BDCR;				// 0x70
+	unsigned int CSR;				// 0x74
+	unsigned int rsd12;				// 0x78	 Reserved address for rsd12
+	unsigned int rsd13;				// 0x7C	 Reserved address for rsd13
+	unsigned int SSCGR;				// 0x80
+	unsigned int PLLI2SCFGR;		// 0x84
+	unsigned int rsd14;				// 0x88	 Reserved address for rsd14
+	unsigned int DCKCFGR;			// 0x8C
+};
+volatile struct rcc *RCC = (volatile struct rcc *)0x40023800;  // RCC Register Base address 0x4002 3800
+/* here type casting for struct name */
+
+/* structure struct struct_tag (or) struct name */
+struct gpio
+{
+	/* structure syntax data type member (or) field (or) elements */
+	/* gpio register 			 offset value  */
+	unsigned int MODER; 			// 0x00
+	unsigned int OTYPER;			// 0x04
+	unsigned int OSPEEDR;			// 0x08
+	unsigned int PUPDR;				// 0x0C
+	unsigned int IDR;				// 0x10
+	unsigned int ODR;				// 0x14
+	unsigned int BSRR;				// 0x18
+	unsigned int LCKR;				// 0x1C
+	unsigned int AFRL;				// 0x20
+	unsigned int AFRH;				// 0x24
+};
+volatile struct gpio *GPIOA = (volatile struct gpio *)0x40020000;  // PORTA  GPIOA Register Base address 0x4002 0000
+/* here type casting for struct name */
+
+/* structure struct struct_tag (or) struct name */
+struct timer
+{
+	/* structure syntax data type member (or) field (or) elements */
+	/* timer register            offset value */
+	unsigned int CR1;               // 0x00
+	unsigned int CR2;               // 0x04
+	unsigned int SMCR;              // 0x08
+	unsigned int DIER;				// 0x0C
+	unsigned int SR;				// 0x10
+	unsigned int EGR;				// 0x14
+	unsigned int CCMR1;             // 0x18
+	unsigned int CCMR2;				// 0x1C
+	unsigned int CCER;              // 0x20
+	unsigned int CNT;               // 0x24
+	unsigned int PSC;				// 0x28
+	unsigned int ARR;				// 0x2C
+	unsigned int RCR;				// 0x30
+	unsigned int CCR1;				// 0x34
+	unsigned int CCR2;       		// 0x38
+	unsigned int CCR3;				// 0x3C
+	unsigned int CCR4;              // 0x40
+	unsigned int BDTR;              // 0x44
+	unsigned int DCR;				// 0x48
+	unsigned int DMAR;				// 0x4C
+};
+volatile struct timer *TIM1 = (volatile struct timer *)0x40010000; // TIMER 1 Register Base address 0x4001 0000
+/* here type casting for struct name */
+
+/* function deceleration */
+void rcc_config(void);
+void peripheral_bus_config(void);
+void gpioa_moder_config(void);
+void pwm_timer1_config(void);
+void rgb_led_blinking(void);
+
+#endif /* ARM_H_ */
---
+++ pwm/pwm_register_timer1_three_channel/Src/main.c
@@ -0,0 +1,191 @@
+/******************************************************************************
+ *                                                                            *
+ *  Author      : Murugan M                                                   *
+ *  Date        : 15-02-2025                                                  *
+ *  File        : pwm_register_timer1_three_channel                           *
+ *  Description : Led blink for PORTA 8th, 9th and 10th pin.                  *
+ *                using for two pheripheral bus AHB1ENR and APB2ENR           *
+ *                pwm three channel for TIM1_CH1, TIM1_CH2, TIM1_CH3          *
+ *                using for HSI : internal clock for 16 MHz      		      *
+ *****************************************************************************/
+
+#include "arm.h"  /* this header file for all register inside there. using for structure is user-defined data type */
+
+/* main function */
+int main(void)
+{
+	/* function call */
+    rcc_config();
+    peripheral_bus_config();
+    gpioa_moder_config();
+    pwm_timer1_config();
+    while(1)
+    {
+    	rgb_led_blinking();
+    }
+}
+
+/* function definition */
+void rcc_config(void)
+{
+	/* select HSI internal clock for 16 MHz ---> Mega hertz. Mega means 10^6 */
+
+	/* Reset clock control register (RCC_CR) */
+
+	/* set and clear for HSION bit */
+	RCC->CR &= ~(1 << 0);
+
+	/* HSION : Internal high-speed clock enable. 0th bit set 1. 1 : HSI oscillator ON */
+	RCC->CR |= (1 << 0);
+
+	/* HSIRDY : Internal high-speed clock ready flag. 1st bit set 1. 1 : HSI oscillator ready */
+	while(!(RCC->CR & (1 << 1)));
+
+	/* SW : System clock switch. 00 : HSI oscillator selected as system clock. 0th bit and 1st bit. set 00 */
+	RCC->CFGR &= ~(3 << 0);
+
+    /* SWS : System clock switch status. 00 : HSI oscillator used as the system clock. 2nd bit and 3rd bit. set 00  */
+	while((RCC->CFGR & (3 << 2)) != (0 << 2));
+}
+
+void peripheral_bus_config(void)
+{
+	/* Advanced High Bus 1. peripheral clock enable register (RCC_AHB1ENR) */
+
+	/* GPIOAEN : port A clock enable */
+	RCC->AHB1ENR |= (1 << 0); // PORT GPIOAEN : port a clock enable
+
+	/* Advanced peripheral Bus 2. peripheral clock enable register (RCC_APB2ENR) */
+
+	/* TIM1EN: TIM1 clock enable */
+	RCC->APB2ENR |= (1 << 0); // TIM1 TIM1EN : timer 1 clock enable
+}
+
+void gpioa_moder_config(void)
+{
+	/* General purpose input and output (GPIO) */
+	/* GPIO port mode register (GPIOA_MODER) */
+
+	/* PORT A MODER8. I select for port A 8th pin for TIMER 1 TIM1_CH1 timer1 first channel */
+	GPIOA->MODER |= (1 << 17); // MODER8 Port A 8th pin. 10 : Alternate function mode
+
+	/* PORT A MODER9. I select for port A 9th pin for TIMER 1 TIM1_CH2 timer1 second channel */
+	GPIOA->MODER |= (1 << 19); // MODER9 Port A 9th pin. 10 : Alternate function mode
+
+	/* PORT A MODER10. I select for port A 10th pin for TIMER 1 TIM1_CH3 timer1 third channel */
+	GPIOA->MODER |= (1 << 21); // MODER10 Port A 10th pin. 10 : Alternate function mode
+
+	/* output speed register (GPIOB_OSPEEDR) */
+
+	GPIOA->OSPEEDR |= (1 << 17);  // 10 : High speed
+	GPIOA->OSPEEDR |= (1 << 19);
+	GPIOA->OSPEEDR |= (1 << 21);
+
+	/* pull-up/pull-down register (GPIOB_PUPDR) */
+	GPIOA->PUPDR |= (1 << 17);  // 10 : Pull-down
+	GPIOA->PUPDR |= (1 << 19);
+	GPIOA->PUPDR |= (1 << 21);
+
+	/* alternate function high register (GPIOB_AFRH) */
+	GPIOA->AFRH |= (1 << 0);   // 0001 : AF1 timer1 pin. PORTA 8th pin for TIM1_CH1 timer1 first channel
+	GPIOA->AFRH |= (1 << 4);   // 0001 : AF1 timer1 pin. PORTA 9th pin for TIM1_CH2 timer1 second channel
+	GPIOA->AFRH |= (1 << 8);   // 0001 : AF1 timer1 pin. PORTA 10th pin for TIM1_CH3 timer1 third channel
+}
+
+void pwm_timer1_config(void)
+{
+	/* capture/compare mode register 1 (TIM1_CCMR1) */
+
+	/* OC1M : output compare 1 mode. output pin select.
+	* 110 : PWM mode 1 - in upcounting, channel 1 is active as long as TIMx_CNT<TIMx_CCR1 else inactive. */
+	TIM1->CCMR1 |= (6 << 4);  // pwm mode 1. OC1M. first pin TIM1_CH1
+
+	/* OC1PE : output compare 1 preload enable.
+	 * 0 : preload register on TIM1_CCR1 disabled. write OC1PE = 0 to disable preload register */
+	TIM1->CCMR1 &= ~(1 << 3);  // OC1PE
+
+	/* OC1FE : output compare 1 fast enable.
+	 * 1 : An active edge on the trigger input acts like a compare match on CC1 output. */
+	TIM1->CCMR1 |= (1 << 2);  // OC1FE
+
+	/* CC1S : capture/compare 1 selection
+	* 00 : CC1 channel is configured as output */
+	TIM1->CCMR1 &= ~(3 << 0); // CC1S
+
+	TIM1->CCMR1 |= (6 << 12); // pwm mode 1. OC2M. second pin TIM1_CH2
+	TIM1->CCMR1 |= (1 << 11); // OC2PE
+	TIM1->CCMR1 |= (1 << 10); // OC2FE
+	TIM1->CCMR1 &= ~(3 << 8); // CC2S
+
+	TIM1->CCMR2 |= (6 << 4);  // pwm mode 1. OC3M. third pin TIM1_CH3
+	TIM1->CCMR2 |= (1 << 3);  // OC3PE
+	TIM1->CCMR2 |= (1 << 2);  // OC3FE
+	TIM1->CCMR2 &= ~(3 << 0); // CC3S
+
+	/* auto-reload register (TIM1_ARR) */
+	TIM1->ARR = 320;
+
+	/* prescaler (TIM1_PSC)
+	 * prescaler value : 1000 */
+	TIM1->PSC = 999;
+
+	/* counter (TIM10_CNT)
+	 * starting with 0 */
+	TIM1->CNT = 0;
+
+	/* TIM1 event generation register (TIM1_EGR) */
+
+	/* UG : update generation. 0th bit set 1
+	 * CC1G: Capture/Compare 1 generation, 1sr bit set 1 */
+	TIM1->EGR |= (3 << 0);
+
+	/* capture/compare enable register (TIM1_CCER) */
+
+	/* CC1E : Capture/compare 1 output enable.
+	 * write CC1E : 1 to enable the output.
+	 * 1 : on - OCC1 signal is output on the corresponding output pin */
+	TIM1->CCER |= (1 << 0);  // CC1E
+
+	/* CC1P : Capture/compare 1 output polarity.
+	 * CC1 channel configured as output
+	 * 0 : OCC1 active high
+	 * write CC1P : 0 to select active high polarity  */
+	TIM1->CCER &= ~(1 << 1); // CC1P
+
+	TIM1->CCER |= (1 << 4);  // CC2E
+	TIM1->CCER &= ~(1 << 5); // CC2P
+
+	TIM1->CCER |= (1 << 8);  // CC3E
+	TIM1->CCER &= ~(1 << 9); // CC3P
+
+	/* break and dead-time register (TIM1_BDTR) */
+	/* MOE : main output enable
+	 * 1: OC and OCN outputs are enabled if their respective enable bits are set (CCxE, CCxNE in TIMx_CCER register). */
+	TIM1->BDTR |= (1 << 15);
+
+	/* control register 1 (TIM10_CR1)
+	 * CEN : counter enabled */
+	TIM1->CR1 |= (1 << 0);
+}
+
+void rgb_led_blinking(void)
+{
+	for (int i = 0; i <= 256; i++)   // led on 16ms. led off 4ms. ms means milliseconds
+	{
+		/* status register (TIM10_SR) */
+		while(!(TIM1->SR & (1 << 0)));  // UIF: Update interrupt flag. 1: Update interrupt pending
+		TIM1->SR &= ~(1 << 0);   // clear the status register. UIF 0th bit and CC1IF bit
+		TIM1->CCR1 = i;           // value load to actual capture/compare 1 register (preload value)
+		TIM1->CCR2 = i;
+		TIM1->CCR3 = i;
+	}
+
+	for (int j = 256; j >= 0; j--)
+	{
+		while(!(TIM1->SR & (1 << 0)));
+		TIM1->SR &= ~(1 << 0);
+		TIM1->CCR1 = j;
+		TIM1->CCR2 = j;
+		TIM1->CCR3 = j;
+	}
+}
